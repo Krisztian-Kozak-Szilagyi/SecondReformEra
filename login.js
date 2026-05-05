81be47ce389ff6474b988232362886a6929d8b28
Karoly Krisztian Kozak-Szilagyi <kozak.karoly.krisztian@gmail.com>
2026-05-05 13:05:05 +0200

    Refactor login authentication to use user array

--- login.js
+++ login.js
@@ -1,11 +1,17 @@
 document.querySelector(".landing-login-button").addEventListener("click", function loginAuthentication() {
-    const loginData = {
-        userName: "Oemer_Isbacar", 
-        password: "Isbacar$42Blue!"
-    };
+    const users = [
+        { userName: "Oemer_Isbacar", password: "Isbacar$42Blue!", redirectUrl: "loginSuccess.html"},
+        { userName: "Luca_Kaupe", password: "KaupeBackpack$platinum99", redirectUrl: "loginSuccess.html"},
+        { userName: "Mirjam_Köckert", password: "Köckert$42Pink", redirectUrl: "loginSuccess.html"}
+    ];
     const enteredName = document.querySelector("#national-ID").value;
     const enteredPassword = document.querySelector("#password").value;
-    if (enteredName === loginData.userName && enteredPassword === loginData.password) {
+
+    const matchedUser = users.find(user =>
+        user.userName === enteredName && user.password === enteredPassword
+    );
+
+    if (matchedUser) {
         window.location.href = "loginSuccess.html";
     } else {
         document.querySelector("#loginAttempt").innerHTML = "Wrong username or password!"
